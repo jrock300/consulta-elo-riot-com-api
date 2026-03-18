04cba6ad150ef8ad4fd0a35513d1fd00492e5547
jrock300 <jrock300@gmail.com>
2026-03-18 03:40:36 -0300

    Adição do ícone do Elo ao lado das informações Nick - Número de PDLs

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,8 +1,22 @@
 const inputNick = document.querySelector('#inputNick');
 const inputTag = document.querySelector('#inputTag');
+const imgElo = document.querySelector('#imgElo');
 const pCompleto = document.querySelector('#pCompleto');
 const btnBuscar = document.querySelector('#btnBuscar');
 const btnLimpar = document.querySelector('#btnLimpar');
+
+const elos = {
+    IRON: 'https://wiki.leagueoflegends.com/en-us/images/Season_2022_-_Iron.png?42102',
+    BRONZE: 'https://wiki.leagueoflegends.com/en-us/images/Season_2022_-_Bronze.png?bbe9f',
+    SILVER: 'https://wiki.leagueoflegends.com/en-us/images/Season_2022_-_Silver.png?46d71',
+    GOLD: 'https://wiki.leagueoflegends.com/en-us/images/Season_2022_-_Gold.png?46d71',
+    PLATINUM: 'https://wiki.leagueoflegends.com/en-us/images/Season_2022_-_Platinum.png?46d71',
+    EMERALD: 'https://wiki.leagueoflegends.com/en-us/images/Season_2023_-_Emerald.png?3c53f',
+    DIAMOND: 'https://wiki.leagueoflegends.com/en-us/images/Season_2022_-_Diamond.png?17b04',
+    MASTER: 'https://wiki.leagueoflegends.com/en-us/images/Season_2022_-_Master.png?4c6ff',
+    GRANDMASTER: 'https://wiki.leagueoflegends.com/en-us/images/Season_2022_-_Grandmaster.png?7505b',
+    CHALLENGER: 'https://wiki.leagueoflegends.com/en-us/images/Season_2022_-_Challenger.png?7505b'
+};
 
 const limpaCampos = () => {
     inputNick.value = "";
@@ -10,6 +24,15 @@ const limpaCampos = () => {
 };
 
 const mostraDados = (nickPlayer, tier, rank, lp) => {
+
+    const tierFormatado = tier?.toUpperCase().trim();
+    const imagem = elos[tierFormatado];
+    if(imagem) {
+        imgElo.src = imagem;
+    } else {
+        imgElo.src = '';
+    }
+
     pCompleto.textContent = `${nickPlayer}: ${tier} ${rank} - ${lp} PDL`;
 };
 
